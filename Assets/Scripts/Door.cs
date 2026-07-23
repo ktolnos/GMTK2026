5845f93dfe845b09490b5549fbe7fa3f1b38c761
maxivitop <m.opryshko@edu.centraluniersity.ru>
2026-07-23 13:37:42 +0300

    SpriteAnimator

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class Door : Interactable
+{
+    public bool isLocked = true;
+    public bool isOpen = false;
+    public bool playerInteractRequested;
+
+    public SpriteAnimator spriteAnimator;
+
+
+    private BoxCollider2D boxCollider;
+    
+
+    void Start()
+    {
+        spriteAnimator = GetComponent<SpriteAnimator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player"))
+        {
+            playerInteractRequested = other.GetComponent<Player>().interactRequested;
+        }
+    }
+
+    public override void Interact()
+    {
+        if (isLocked)
+        {
+            Debug.Log("Door is locked");
+        }
+        else
+        {
+            isOpen = !isOpen;
+            Debug.Log("Door is open");
+            boxCollider.enabled = isOpen;
+
+        }
+    }
+
+    void Update()
+    {
+        if (playerInteractRequested)
+        {
+            Interact();
+            playerInteractRequested = false;
+        }
+    }
+
+}
---
+++ Assets/Scripts/SpriteAnimator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class SpriteAnimator: MonoBehaviour
+{
+    public new Animation animation;
+    public bool autoplay = true;
+    public bool loop = true;
+    public SpriteRenderer spriteRenderer;
+    private bool pause;
+    private void Awake()
+    {
+        if (spriteRenderer == null)
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+    }
+
+    private void Start()
+    {
+        if (!loop && autoplay)
+        {
+            PlayOnce();
+        }
+    }
+
+    private void Update()
+    {
+        if (animation.frames.Length == 0 || !autoplay || pause || !loop)
+        {
+            return;
+        }
+        spriteRenderer.sprite = animation.frames[(int)(Time.time *  animation.fps) % animation.frames.Length];
+    }
+    
+    [Serializable]
+    public class Animation
+    {
+        public Sprite[] frames;
+        public float fps = 10f;
+        
+        public Animation(Sprite[] frames, float fps)
+        {
+            this.frames = frames;
+            this.fps = fps;
+        }
+    }
+
+    public void PlayOnce()
+    {
+        PlayOnce(animation);
+    }
+
+    public void PlayOnce(Animation anim)
+    {
+        StartCoroutine(PlayOnceCoroutine(anim));
+    }
+
+    private IEnumerator PlayOnceCoroutine(Animation anim)
+    {        
+        pause = true;
+        foreach (var animationFrame in anim.frames)
+        {
+            spriteRenderer.sprite = animationFrame;
+            yield return new WaitForSeconds(1f / anim.fps);
+        }
+        pause = false;
+    }
+}
