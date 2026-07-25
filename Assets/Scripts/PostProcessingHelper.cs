35db69f46fcfd86f7edae78b64139fbfc9b71cce
eop <ktolnos@mail.com>
2026-07-25 19:15:04 -0400

    Post processing for recoding

---
+++ Assets/Scripts/PostProcessingHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
+
+public class PostProcessingHelper: MonoBehaviour
+{
+    public Renderer2DData rendererData;
+    public Volume mainVolume;
+    private ScriptableRendererFeature crt;
+    private ChromaticAberration chrom;
+
+    public Image recordingImage;
+
+    private void Start()
+    {
+        foreach (var rendererDataRendererFeature in rendererData.rendererFeatures)
+        {
+            if (rendererDataRendererFeature.name == "CRT")
+            {
+                crt = rendererDataRendererFeature;
+            }
+        }
+
+        mainVolume.profile.TryGet(out chrom);
+    }
+
+    private void Update()
+    {
+        var postEnabled = !GM.ActivePlayer.isControlled && GM.isPlaying;
+        crt.SetActive(postEnabled);
+        chrom.active = postEnabled;
+        recordingImage.enabled = postEnabled;
+
+        var color = recordingImage.color;
+        color.a = (Mathf.Sin(Time.time * Mathf.PI) + 1f) / 2;
+        recordingImage.color = color;
+    }
+        
+}
