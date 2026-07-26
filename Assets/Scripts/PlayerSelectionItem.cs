d872f500d21e8de00f9f6dec0d90c79fd00a53bf
eop <ktolnos@mail.com>
2026-07-26 07:29:01 -0400

    Player selection tweaks

--- Assets/Scripts/PlayerSelectionItem.cs
+++ Assets/Scripts/PlayerSelectionItem.cs
@@ -2,14 +2,17 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class PlayerSelectionItem: MonoBehaviour
+public class PlayerSelectionItem: MonoBehaviour, IPointerEnterHandler
 {
     public Button button;
     public Image image;
     public TextMeshProUGUI playerName;
     public TextMeshProUGUI playerDescription;
+    
+    private Player player;
 
     private void Awake()
     {
@@ -28,8 +31,15 @@ public class PlayerSelectionItem: MonoBehaviour
                 GM.StartLoop();
             }
         );
+        this.player = player;
         image.sprite = player.icon;
         playerName.text = player.playerName;
         playerDescription.text = player.description;
     }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        GM.SelectPlayer(player);
+    }
+
 }
