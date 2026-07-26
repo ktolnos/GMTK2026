e46ab168287dd9776a3e4d0d9100926a2754ba60
Daniil Gaponov <gaponov.daniil@gmail.com>
2026-07-26 17:11:26 +0300

    sounds pt. XIV (main menu theme)

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,6 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private AudioContainer music;
+
+    private void Start() {
+        AudioManager.I.PlayMusic(music);
+    }
+
     public void StartGame()
     {
         Debug.Log("Starting Game");
